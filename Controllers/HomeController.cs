0f7285edfefb8d8429630034bf153e79f5f5af7e
andre-prado <andr3prad0@gmail.com>
2022-02-20 21:44:26 -0300

    API concluida a fim de estudos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using Todo.Models;
 namespace Todo.Controllers
 {
     [ApiController]
-    [Route("/")]
+    [Route("/todo")]
     public class HomeController : ControllerBase
     {
         AppDbContext _context;
@@ -15,16 +15,56 @@ namespace Todo.Controllers
         }
 
         [HttpGet]
-        public List<TodoModel> Get()
+        public IActionResult Get()
+            => Ok(_context.Todos.ToList());
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
         {
-            return _context.Todos.ToList();
+            var todo = _context.Todos.FirstOrDefault(x => x.Id == id);
+            if (todo == null)
+                return NotFound();
+
+            return Ok(todo);
         }
 
-        public TodoModel Post([FromBody] TodoModel todo)
+        [HttpPost]
+        public IActionResult Post([FromBody] TodoModel todo)
         {
             _context.Todos.Add(todo);
             _context.SaveChanges();
-            return todo;
+            return Created($"/{todo.Id}", todo);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Put([FromRoute] int id, [FromBody] TodoModel todoUpdated)
+        {
+            var todo = _context.Todos.FirstOrDefault(x => x.Id == id);
+            if (todo == null)
+                return NotFound();
+
+
+            todo.Done = todoUpdated.Done;
+            todo.Title = todoUpdated.Title;
+
+            _context.Update(todo);
+            _context.SaveChanges();
+
+            return Ok(todo);
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var todo = _context.Todos.FirstOrDefault(x => x.Id == id);
+            if (todo == null)
+                return NotFound();
+
+            _context.Remove(todo);
+            _context.SaveChanges();
+
+            return Ok(todo);
         }
     }
 }
